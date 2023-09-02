d8f11c7c63237f8f882eec8dd573ad012e197979
Aman Bhardwaj <107456156+aman1999hopper@users.noreply.github.com>
2023-09-02 16:06:11 +0530

    Add files via upload

---
+++ Ques1.js
@@ -0,0 +1,7 @@
+const A = [1,2,3,4,5];
+console.log('[A]:', A);
+// output: "[A]:" Array [1,2,3,4,5]
+
+const reversed = A.reverse();
+console.log('[B]:', reversed);
+//output: "reversed:" Array [5,4,3,2,1]
