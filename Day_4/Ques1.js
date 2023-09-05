a293344728b7e2ea3e6e33653c716adf9ee985fb
aman1999hopper <amanbhardwaj5931@gmail.com>
2023-09-05 20:20:47 +0530

    Add files

--- Day_4/Ques1.js
+++
@@ -1,9 +0,0 @@
-// to sort an array it will show in ascending order.
-const A = [0, 1, 2, 3, 4, 5];
-const B = [5, 4, 3, 2, 1, 0];
-
-A.sort((a, b) => a - b);
-B.sort((a, b) => a - b);
-
-console.log("Sorted A:", A);
-console.log("Sorted B:", B);
