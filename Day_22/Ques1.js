475ce45a6d6f3b44e35682fc15c214257f058598
Aman Bhardwaj <amanbhardwaj5931@gmail.com>
2023-09-26 10:14:59 +0530

    Day_22 file

---
+++ Day_22/Ques1.js
@@ -0,0 +1,17 @@
+// Array is sorted or not. If array is sorted return true if not return false 
+
+function isSorted(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[i])
+          return false;
+      }
+    }
+  
+    return true;
+  }
+  
+  const arr = [1, 2, 3, 4, 5];
+  const ans = isSorted(arr);
+  if (ans) console.log("True");
+  else console.log("False");
